c2006e7228419a5a5ee31232e236ebf185c1f500
Fernando <Fernando@DESKTOP-898U8DI>
2019-12-11 14:38:25 -0500

    Prueba para repositorio en java

---
+++ src/repositorio1/Repositorio1.java
@@ -0,0 +1,14 @@
+
+package repositorio1;
+
+
+public class Repositorio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
